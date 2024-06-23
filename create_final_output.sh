7121f784fb5550e0328641b7a26e8ffe8e7feb12
mkmksto <119323664+mkmksto@users.noreply.github.com>
2024-06-23 17:47:03 +0800

    shell script: also create zip file

--- create_final_output.sh
+++ create_final_output.sh
@@ -4,3 +4,5 @@ cp -r ./notebooks/*.ipynb ./final_output/notebooks/
 cp -r ./output/* ./final_output/output/
 cp -r ./raw-data/* ./final_output/raw-data/
 cp ./report_backup/*.pdf ./final_output/report/
+
+zip -r final_output_cantor_quinto.zip ./final_output
